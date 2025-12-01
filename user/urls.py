93853eac10d5b11a0af04915c84ae1e90a12af14
FransiPatel <fransip@zignuts.com>
2025-12-01 17:07:45 +0530

    Updated the user app name to users.

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegisterUser, LoginUser, ProfileUser
-
-urlpatterns = [
-    path("register/", RegisterUser.as_view(), name="register"),
-    path("login/", LoginUser.as_view(), name="login"),
-    path("profile/", ProfileUser.as_view(), name="profile"),
-]
